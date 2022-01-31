95a57730a64664f16459c0ad42448a6c0db131f3
LucasRomanGervasi <gervasilucas22@gmail.com>
2022-01-31 01:19:38 -0300

    add challenge

---
+++ menu.js
@@ -0,0 +1,25 @@
+/*MENU ITEMS 2*/
+function mostrarItems(){
+    document.getElementById('ocultaritems').style.display = 'none'
+}
+
+/*MENU ITEMS 3*/
+function mouseOver(){
+    document.getElementById('sidebarnaranja').style.backgroundColor = '#e61717e2'
+}
+function mouseOut(){
+    document.getElementById('sidebarnaranja').style.backgroundColor = '#CA5310 '
+}
+
+/*MENU ITEMS 4*/ 
+function mostrarTexto(){
+    document.getElementById('holamundo').style.display = "block"
+}
+
+/*Menu Hamburguesa*/
+const btn_menu = document.querySelector(".btn_menu")
+const menus = document.querySelector(".menus")
+
+btn_menu.addEventListener("click", () => {
+    menus.classList.toggle("menus_visible")
+})
